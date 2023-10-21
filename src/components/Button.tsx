2aa807858fccd8cecc50eae9d9dc36d41ee631e9
Daniel Filat <filatdaniel98@gmail.com>
2023-10-21 14:43:40 +0300

    chore: sets up cva for Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,17 +1,27 @@
 import { type ComponentProps } from 'react'
-import { css } from 'styled-system/css'
+import { css, cva, cx } from 'styled-system/css'
 import { type SystemStyleObject } from 'styled-system/types'
 
-const buttonStyle: SystemStyleObject = {
-	rounded: 'full',
-	color: 'white',
-	bg: 'blue.500',
-}
+export const button = cva({
+	base: {
+		display: 'flex',
+	},
+	variants: {
+		visual: {
+			solid: { bg: 'red.200', color: 'white' },
+			outline: { borderWidth: '1px', borderColor: 'red.200' },
+		},
+		size: {
+			sm: { padding: '4', fontSize: '12px' },
+			lg: { padding: '8', fontSize: '24px' },
+		},
+	},
+})
 
 export const Button = ({
 	css: _css,
 	...props
 }: ComponentProps<'button'> & { css?: SystemStyleObject }) => {
-	const className = css(buttonStyle, _css)
+	const className = cx(button({ visual: 'outline', size: 'lg' }), css(_css))
 	return <button className={className} {...props}></button>
 }
