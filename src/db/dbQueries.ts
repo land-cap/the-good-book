7773cb091314d0c97df2425ab9d9cb3452693e42
Daniel Filat <filatdaniel98@gmail.com>
2023-11-15 08:14:16 +0200

    11 add caching for chapter data queries (#15)
    * feat: adds withCache HOF and uses it for all DB queries
    
    * replaces in-memory cache with file system cache
    
    * redirects from root route to Genesis 1
    
    * chore: test ENVIRONMENT env variable
    
    * feat: sets up cache to use memory when deployed and file system on local

--- src/db/dbQueries.ts
+++ src/db/dbQueries.ts
@@ -1,11 +1,25 @@
 import { dbClient } from '~/db/dbClient'
+import { createFileSystemCache, createMemoryCache, withCache } from '~/helpers'
 
-export const getBookName = async (bookCode: string) =>
+const useMemoryCache = process.env.USE_MEMORY_CACHE === 'true'
+
+const getBookName = async (bookCode: string) =>
 	dbClient.vdc_book_name.findFirst({ where: { book: { code: bookCode } } })
 
-export const getChapter = async (bookCode: string, chapter: number) => {
+export const getBookNameWithCache = withCache(
+	getBookName,
+	useMemoryCache ? createMemoryCache() : createFileSystemCache(),
+) as typeof getBookName
+
+const getChapter = async (bookCode: string, chapter: number) => {
+	console.log('getChapter called with', { bookCode, chapter })
 	const book = await dbClient.book.findFirst({ where: { code: bookCode } })
 	return dbClient.vdc_chapter.findFirst({
 		where: { book_id: book?.id, chapter },
 	})
 }
+
+export const getChapterWithCache = withCache(
+	getChapter,
+	useMemoryCache ? createMemoryCache() : createFileSystemCache(),
+) as typeof getChapter
---
+++ src/helpers/withCache.ts
@@ -0,0 +1,49 @@
+import { Cache } from 'file-system-cache'
+import NodeCache from 'node-cache'
+
+export type CachePlugin<T> = {
+	get: (key: string) => Promise<T | undefined>
+	set: (key: string, value: T) => void
+}
+
+export const createMemoryCache = <T>(
+	options?: ConstructorParameters<typeof NodeCache>[0],
+): CachePlugin<T> => {
+	const cache = new NodeCache(options ?? { stdTTL: 60 * 60 })
+
+	return {
+		get: async (key) => cache.get(key),
+		set: (key, value) => cache.set(key, value),
+	}
+}
+
+export const createFileSystemCache = <T>(
+	options?: ConstructorParameters<typeof Cache>[0],
+): CachePlugin<T> => {
+	const cache = new Cache(options ?? { ttl: 60 * 60 })
+
+	return {
+		get: async (key) => cache.get(key),
+		set: (key, value) => {
+			cache.set(key, value).catch((err) => {
+				throw err
+			})
+		},
+	}
+}
+
+// TODO: fix return type issue
+export const withCache =
+	<Args extends never[], Result, Fn extends (...args: Args) => Promise<Result>>(
+		fn: Fn,
+		cache: CachePlugin<Result> = createMemoryCache<Result>(),
+	) =>
+	async (...args: Parameters<Fn>): Promise<Result> => {
+		const cachedValue = await cache.get(args.toString())
+		if (cachedValue) {
+			return cachedValue
+		}
+		const result = await fn(...args)
+		cache.set(args.toString(), result)
+		return result
+	}
