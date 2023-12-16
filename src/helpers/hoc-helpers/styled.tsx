44c5d9eede15a2eecdf8bd7d0e2ba14319623715
Daniel Filat <filatdaniel98@gmail.com>
2023-12-16 19:16:00 +0200

    fix: type issue in styled hoc helper (with function overloading)

--- src/helpers/hoc-helpers/styled.tsx
+++ src/helpers/hoc-helpers/styled.tsx
@@ -1,44 +1,32 @@
-import Link from 'next/link'
 import { type JSX, type ReactNode } from 'react'
 import { twMerge } from 'tailwind-merge'
 
-type IntrinsicElement = keyof JSX.IntrinsicElements
+type TIntrinsicElement = keyof JSX.IntrinsicElements
 
-type Component<P extends { className?: string }> = (props: P) => ReactNode
+type TComponent<P extends { className?: string }> = (props: P) => ReactNode
 
-type StyledItem<
-	ElementOrComponent extends IntrinsicElement | Component<never>,
-> = ElementOrComponent extends Component<infer P>
-	? P
-	: //@ts-ignore
-	  JSX.IntrinsicElements[ElementOrComponent]
+type TStyledIntrinsicElement<E extends TIntrinsicElement> = (
+	className: string,
+) => (props: JSX.IntrinsicElements[E]) => ReactNode
 
-type A = StyledItem<'a'>
+type TStyledComponent<P extends { className?: string }> = (
+	className: string,
+) => (props: P) => ReactNode
 
-type B = StyledItem<typeof Link>
+export function styled<E extends TIntrinsicElement>(
+	Element: E,
+): TStyledIntrinsicElement<E>
 
-export const styled =
-	<
-		P extends { className?: string },
-		ElementOrComponent extends IntrinsicElement | Component<P>,
-	>(
-		ElementOrComponent: ElementOrComponent,
-	) =>
-	(className: string) =>
+export function styled<P extends { className?: string }>(
+	Component: TComponent<P>,
+): TStyledComponent<P>
+
+// TODO: improve typing with conditional types and remove overloading
+export function styled<P extends { className?: string }>(
+	Component: TComponent<P>,
+) {
 	//eslint-disable-next-line react/display-name
-	(props: StyledItem<ElementOrComponent>) => (
-		// TODO: fix type issue
-		//@ts-ignore
-		<ElementOrComponent
-			{...props}
-			className={twMerge(className, props.className)}
-		/>
+	return (className: string) => (props: P) => (
+		<Component {...props} className={twMerge(className, props.className)} />
 	)
-
-const StyledLink = styled(Link)
-
-const SimpleComponent = ({ className }: { className?: string }) => (
-	<div className={className}>simple component</div>
-)
-
-const StyledSimpleCoponent = styled(SimpleComponent)
+}
