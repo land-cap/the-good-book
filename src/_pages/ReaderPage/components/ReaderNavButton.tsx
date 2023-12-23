3a53e0a455de7e04a9a6f294266bfd176413c07a
Daniel Filat <filatdaniel98@gmail.com>
2023-12-23 18:52:37 +0200

    Revert "rollbacks changes for making Lucid icons sharp"
    This reverts commit 9e23c1c08adeadb5f44caac64be906fb6520b0ba.

--- src/_pages/ReaderPage/components/ReaderNavButton.tsx
+++ src/_pages/ReaderPage/components/ReaderNavButton.tsx
@@ -1,5 +1,7 @@
 import { ChevronLeft, ChevronRight } from 'lucide-react'
 import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
+import { iconCls } from '~/components'
 
 export const ReaderNavButton = ({
 	href,
@@ -14,9 +16,19 @@ export const ReaderNavButton = ({
 		className="flex h-14 w-14 place-content-center place-items-center text-fg transition duration-quick ease-in-out hover:bg-bgSubtle active:text-fgSubtle"
 	>
 		{direction === 'left' ? (
-			<ChevronLeft size={40} strokeWidth={2} absoluteStrokeWidth />
+			<ChevronLeft
+				size={40}
+				strokeWidth={2}
+				absoluteStrokeWidth
+				className={twMerge(iconCls)}
+			/>
 		) : (
-			<ChevronRight size={40} strokeWidth={2} absoluteStrokeWidth />
+			<ChevronRight
+				size={40}
+				strokeWidth={2}
+				absoluteStrokeWidth
+				className={twMerge(iconCls)}
+			/>
 		)}
 	</Link>
 )
