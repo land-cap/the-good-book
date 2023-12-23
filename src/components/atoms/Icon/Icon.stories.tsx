786612c526980d3c1f2aac4714bffbaffd6d85fa
Daniel Filat <filatdaniel98@gmail.com>
2023-12-23 19:29:57 +0200

    makes icons thinner in dark mode using grade instead of weight axis

--- src/components/atoms/Icon/Icon.stories.tsx
+++ src/components/atoms/Icon/Icon.stories.tsx
@@ -15,7 +15,12 @@ const iconSizeRange = (
 	<div className="flex flex-row items-center gap-8">
 		{Array.from(iconSizeList).map((size) => (
 			<div key={size} className="border border-neutral-200">
-				<Icon key={size} name="bolt" size={size} className="text-neutral-500" />
+				<Icon
+					key={size}
+					name="square"
+					size={size}
+					className="text-neutral-500"
+				/>
 			</div>
 		))}
 	</div>
