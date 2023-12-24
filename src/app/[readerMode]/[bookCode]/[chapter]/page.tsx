f09b893ea2377bcc2d59addbef7d48c9674fd00d
Daniel Filat <filatdaniel98@gmail.com>
2023-12-24 17:26:23 +0200

    Update page.tsx

--- src/app/[readerMode]/[bookCode]/[chapter]/page.tsx
+++ src/app/[readerMode]/[bookCode]/[chapter]/page.tsx
@@ -1,1 +1,9 @@
+export const generateStaticParams = () => [
+{ readerMode: "study", bookCode: "mat", chapter: "1"},
+{ readerMode: "study", bookCode: "mat", chapter: "2"},
+{ readerMode: "study", bookCode: "mat", chapter: "3"},
+{ readerMode: "study", bookCode: "mat", chapter: "4"},
+
+]
+
 export { ReaderPage as default } from '~/_pages'
