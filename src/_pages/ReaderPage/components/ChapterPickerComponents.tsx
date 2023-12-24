98c6246866587410f3fced027bf92ace45e98b5e
Daniel Filat <filatdaniel98@gmail.com>
2023-12-24 10:55:06 +0200

    Update ChapterPickerComponents.tsx

--- src/_pages/ReaderPage/components/ChapterPickerComponents.tsx
+++ src/_pages/ReaderPage/components/ChapterPickerComponents.tsx
@@ -44,7 +44,7 @@ export const ChapterPickerHeader = () => (
 	<ChapterPickerHeaderContainer>
 		<Select.ItemGroupLabel
 			htmlFor="book"
-			className="text-sm font-bold sm:text-base"
+			className="text-sm"
 		>
 			Book
 		</Select.ItemGroupLabel>
