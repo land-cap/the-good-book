fea08e11cc3f4ed58ac96191daec744b513ece9a
Daniel Filat <filatdaniel98@gmail.com>
2023-12-24 10:53:45 +0200

    Update Header.tsx

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -11,7 +11,7 @@ const NavBarContainer = wChildren(({ children }) => (
 	</div>
 ))
 
-const Logo = wClassName(Link)('font-bold text-sm sm:text-base')
+const Logo = wClassName(Link)('font-bold')
 
 export const Header = () => (
 	<NavBarContainer>
