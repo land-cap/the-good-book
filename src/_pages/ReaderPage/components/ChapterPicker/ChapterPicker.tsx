2932adf5d908d4adae670ca71377a4ca3884a2be
Daniel Filat <filatdaniel98@gmail.com>
2023-12-27 23:07:58 +0200

    converts existing chapter picker styles to panda

--- src/_pages/ReaderPage/components/ChapterPicker/ChapterPicker.tsx
+++ src/_pages/ReaderPage/components/ChapterPicker/ChapterPicker.tsx
@@ -1,14 +1,15 @@
 'use client'
 
 import { Dialog, Portal } from '@ark-ui/react'
+import { css } from 'styled-system/css'
 import { ChapterPickerTrigger } from './ChapterPickerTrigger'
 
 export const ChapterPicker = () => (
 	<Dialog.Root>
 		<ChapterPickerTrigger>Open Dialog</ChapterPickerTrigger>
 		<Portal>
-			<Dialog.Positioner className="fixed inset-0">
-				<Dialog.Content className="h-full bg-bgSurface">
+			<Dialog.Positioner className={css({ inset: '0', position: 'fixed' })}>
+				<Dialog.Content className={css({ bg: 'bg.surface', h: 'full' })}>
 					<Dialog.Title>Dialog Title</Dialog.Title>
 					<Dialog.Description>Dialog Description</Dialog.Description>
 					<Dialog.CloseTrigger>Close</Dialog.CloseTrigger>
