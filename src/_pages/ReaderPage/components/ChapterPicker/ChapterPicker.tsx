80ea2c14705800cf2b5b24cc5c1387ba26d075a4
Daniel Filat <filatdaniel98@gmail.com>
2023-12-28 00:47:31 +0200

    adds chapter picker container component

--- src/_pages/ReaderPage/components/ChapterPicker/ChapterPicker.tsx
+++ src/_pages/ReaderPage/components/ChapterPicker/ChapterPicker.tsx
@@ -1,20 +1,18 @@
 'use client'
 
 import { Dialog, Portal } from '@ark-ui/react'
-import { css } from 'styled-system/css'
+import { ChapterPickerContainer } from '~/_pages/ReaderPage/components/ChapterPicker/ChapterPickerContainer'
 import { ChapterPickerTrigger } from './ChapterPickerTrigger'
 
 export const ChapterPicker = () => (
 	<Dialog.Root>
 		<ChapterPickerTrigger>Open Dialog</ChapterPickerTrigger>
 		<Portal>
-			<Dialog.Positioner className={css({ inset: '0', position: 'fixed' })}>
-				<Dialog.Content className={css({ bg: 'bg.surface', h: 'full' })}>
-					<Dialog.Title>Dialog Title</Dialog.Title>
-					<Dialog.Description>Dialog Description</Dialog.Description>
-					<Dialog.CloseTrigger>Close</Dialog.CloseTrigger>
-				</Dialog.Content>
-			</Dialog.Positioner>
+			<ChapterPickerContainer>
+				<Dialog.Title>Dialog Title</Dialog.Title>
+				<Dialog.Description>Dialog Description</Dialog.Description>
+				<Dialog.CloseTrigger>Close</Dialog.CloseTrigger>
+			</ChapterPickerContainer>
 		</Portal>
 	</Dialog.Root>
 )
---
+++ src/_pages/ReaderPage/components/ChapterPicker/ChapterPickerContainer.tsx
@@ -0,0 +1,29 @@
+import { Dialog } from '@ark-ui/react'
+import { type ReactNode } from 'react'
+import { css } from 'styled-system/css'
+import { macrogrid } from 'styled-system/patterns'
+
+export const ChapterPickerContainer = ({
+	children,
+}: {
+	children: ReactNode
+}) => (
+	<Dialog.Positioner className={css({ inset: '0', position: 'fixed' })}>
+		<Dialog.Content
+			className={css({
+				h: 'full',
+				zIndex: '20',
+			})}
+		>
+			<div
+				className={macrogrid({
+					bg: 'bg.surface',
+					h: 'full',
+					overflow: 'hidden',
+				})}
+			>
+				{children}
+			</div>
+		</Dialog.Content>
+	</Dialog.Positioner>
+)
