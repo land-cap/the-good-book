eefea399938fcee9cdadcae10da5cbf069d2e58e
Daniel Filat <filatdaniel98@gmail.com>
2023-12-28 05:34:53 +0200

    computes chapter list padding from list item size

--- src/_pages/ReaderPage/components/ChapterPicker/ChapterPickerChapterListItem.tsx
+++ src/_pages/ReaderPage/components/ChapterPicker/ChapterPickerChapterListItem.tsx
@@ -1,31 +1,38 @@
 import Link from 'next/link'
-import { type ReactNode } from 'react'
+import { type ForwardedRef, forwardRef, type ReactNode } from 'react'
 import { css } from 'styled-system/css'
 import { center } from 'styled-system/patterns'
 
-export const ChapterPickerChapterListItem = ({
-	children,
-	href,
-}: {
-	children: ReactNode
-	href: string
-}) => (
-	<li
-		className={css({
-			_active: { color: 'fg.subtle' },
-			_hover: { bg: 'bg.subtle' },
-			aspectRatio: '1/1',
-			placeContent: 'center',
-			placeItems: 'center',
-			position: 'relative',
-			transition: 'colors',
-			transitionDuration: 'fast',
-			transitionTimingFunction: 'ease-in-out',
-			w: 'full',
-		})}
-	>
-		<Link href={href} className={center({ inset: 0, position: 'absolute' })}>
-			{children}
-		</Link>
-	</li>
+//eslint-disable-next-line react/display-name
+export const ChapterPickerChapterListItem = forwardRef(
+	(
+		{
+			children,
+			href,
+		}: {
+			children: ReactNode
+			href: string
+		},
+		ref: ForwardedRef<HTMLLIElement>,
+	) => (
+		<li
+			ref={ref}
+			className={css({
+				_active: { color: 'fg.subtle' },
+				_hover: { bg: 'bg.subtle' },
+				aspectRatio: '1/1',
+				placeContent: 'center',
+				placeItems: 'center',
+				position: 'relative',
+				transition: 'colors',
+				transitionDuration: 'fast',
+				transitionTimingFunction: 'ease-in-out',
+				w: 'full',
+			})}
+		>
+			<Link href={href} className={center({ inset: 0, position: 'absolute' })}>
+				{children}
+			</Link>
+		</li>
+	),
 )
