545f9015d9f0a2b89154f00e6799ef614e465346
Daniel Filat <filatdaniel98@gmail.com>
2023-12-30 16:29:56 +0200

    64 improve supabase db schema (#65)
    * improves database schema
    
    * keeps chapter list item highlighted after clicked and unmounts chapter picker on exit

--- src/components/organisms/ReaderControls/ChapterPicker/ChapterListItem_ChapterPicker.tsx
+++ src/components/organisms/ReaderControls/ChapterPicker/ChapterListItem_ChapterPicker.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { type ForwardedRef, forwardRef, type ReactNode } from 'react'
+import { type ForwardedRef, forwardRef, type ReactNode, useState } from 'react'
 import { css, cx } from 'styled-system/css'
 import { center } from 'styled-system/patterns'
 
@@ -15,33 +15,42 @@ const ChapterListItem_ChapterPicker = forwardRef(
 			isCurrChapter: boolean
 		},
 		ref: ForwardedRef<HTMLLIElement>,
-	) => (
-		<li
-			ref={ref}
-			className={cx(
-				css({
-					_active: { bg: 'bg.subtle', color: 'fg.subtle' },
-					_canHover: {
-						_hover: { bg: 'bg.subtle' },
-						transitionDelay: 'fastest',
-					},
-					aspectRatio: '1/1',
-					placeContent: 'center',
-					placeItems: 'center',
-					position: 'relative',
-					transition: 'colors',
-					transitionDuration: 'fastest',
-					transitionTimingFunction: 'ease-out',
-					w: 'full',
-				}),
-				isCurrChapter && css({ fontWeight: 'bold' }),
-			)}
-		>
-			<Link href={href} className={center({ inset: 0, position: 'absolute' })}>
-				{children}
-			</Link>
-		</li>
-	),
+	) => {
+		const [isSelected, setIsSelected] = useState(false)
+
+		return (
+			<li
+				ref={ref}
+				className={cx(
+					css({
+						_active: { bg: 'bg.subtle', color: 'fg.subtle' },
+						_canHover: {
+							_hover: { bg: 'bg.subtle' },
+							transitionDelay: 'fastest',
+						},
+						aspectRatio: '1/1',
+						placeContent: 'center',
+						placeItems: 'center',
+						position: 'relative',
+						transition: 'colors',
+						transitionDuration: 'fastest',
+						transitionTimingFunction: 'ease-out',
+						w: 'full',
+					}),
+					isCurrChapter && css({ fontWeight: 'bold' }),
+					isSelected && css({ bg: 'bg.subtle' }),
+				)}
+			>
+				<Link
+					href={href}
+					onClick={() => setIsSelected(true)}
+					className={center({ inset: 0, position: 'absolute' })}
+				>
+					{children}
+				</Link>
+			</li>
+		)
+	},
 )
 
 ChapterListItem_ChapterPicker.displayName = 'ChapterListItem_ChapterPicker'
