0d1a4fd291cf44bc1a6eae020f624341f4e7d9cf
Daniel Filat <filatdaniel98@gmail.com>
2023-12-30 14:04:04 +0200

    generates static params only on prod environment

--- src/db/dbQueries.ts
+++ src/db/dbQueries.ts
@@ -7,31 +7,38 @@ import {
 
 const useMemoryCache = process.env.USE_MEMORY_CACHE === 'true'
 
-const getBook = async (bookCode: string) =>
-	dbClient.book.findFirst({
-		include: { book_name: true },
-		where: { code: bookCode },
-	})
-
 export const getBookList = async () =>
 	dbClient.book.findMany({
 		include: { book_name: true },
 	})
 
-export const getBookWithCache = withCacheAsync(
-	getBook,
+export const getBookListWithCache = withCacheAsync(
+	getBookList,
 	useMemoryCache ? createMemoryCache() : createFileSystemCache(),
 )
 
 export type TBook = Awaited<ReturnType<typeof getBookList>>[0]
 
+const getChapterList = async () => dbClient.chapter.findMany()
+
+export const getChapterListWithCache = withCacheAsync(
+	getChapterList,
+	useMemoryCache ? createMemoryCache() : createFileSystemCache(),
+)
+
 const getChapter = async (bookCode: string, chapter: number) => {
-	const book = await dbClient.book.findFirst({
-		where: { code: bookCode },
-	})
-	return dbClient.chapter.findFirst({
-		where: { book_id: book?.id, chapter },
-	})
+	const bookList = await getBookListWithCache()
+
+	const book = bookList.find((book) => book.code === bookCode)
+
+	if (!book) throw new Error('Book not found')
+
+	const chapterList = await getChapterListWithCache()
+
+	return chapterList.find(
+		({ book_id, chapter: _chapter }) =>
+			book_id === book.id && _chapter === chapter,
+	)
 }
 
 export const getChapterWithCache = withCacheAsync(
