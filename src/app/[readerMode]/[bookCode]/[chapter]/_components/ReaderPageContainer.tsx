3998bf5a0c526717d4c10eb7467d9ce8c80c83ac
Daniel Filat <filatdaniel98@gmail.com>
2024-01-01 14:50:24 +0200

    [WIP]

--- src/app/[readerMode]/[bookCode]/[chapter]/_components/ReaderPageContainer.tsx
+++ src/app/[readerMode]/[bookCode]/[chapter]/_components/ReaderPageContainer.tsx
@@ -1,13 +1,19 @@
 'use client'
 
 import { useSetAtom } from 'jotai'
-import { type ReactNode } from 'react'
+import { type ReactNode, useEffect } from 'react'
 import { macrogrid } from 'styled-system/patterns'
 
 import { readerPageContainerElAtom } from '../_readerPage.state'
 
 export const ReaderPageContainer = ({ children }: { children: ReactNode }) => {
 	const setReaderPageContainerEl = useSetAtom(readerPageContainerElAtom)
+
+	useEffect(() => {
+		document.addEventListener('scroll', (...args) => {
+			console.log(args)
+		})
+	}, [])
 
 	return (
 		<div
