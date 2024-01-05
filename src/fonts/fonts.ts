ad19bc111dba4c88f8372b70ee2e43e8e10c0abb
Daniel Filat <filatdaniel98@gmail.com>
2024-01-05 11:24:43 +0200

    adds other formats to font definition

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -3,13 +3,39 @@ import localFont from 'next/font/local'
 export const fontFranklin = localFont({
 	src: [
 		{
+			path: './franklin-regular/franklin-regular.eot',
+			weight: '400',
+		},
+		{
+			path: './franklin-regular/franklin-regular.ttf',
+			weight: '400',
+		},
+		{
 			path: './franklin-regular/franklin-regular.woff',
+			weight: '400',
+		},
+		{
+			path: './franklin-regular-italic/franklin-regular-italic.eot',
+			style: 'italic',
+			weight: '400',
+		},
+		{
+			path: './franklin-regular-italic/franklin-regular-italic.ttf',
+			style: 'italic',
 			weight: '400',
 		},
 		{
 			path: './franklin-regular-italic/franklin-regular-italic.woff',
 			style: 'italic',
 			weight: '400',
+		},
+		{
+			path: './franklin-demi/franklin-demi.eot',
+			weight: '800',
+		},
+		{
+			path: './franklin-demi/franklin-demi.ttf',
+			weight: '800',
 		},
 		{
 			path: './franklin-demi/franklin-demi.woff',
