174a58a8c22aea3a4627bf41b0827b564c6fda85
Daniel Filat <filatdaniel98@gmail.com>
2024-01-19 15:31:56 +0200

    creates hook for setting up client state

--- src/_pages/ReaderPage/HydrateReaderPageAtoms.ts
+++ src/_pages/ReaderPage/HydrateReaderPageAtoms.ts
@@ -1,10 +1,12 @@
 'use client'
 
-import { useSetAtom } from 'jotai'
+import { useAtom } from 'jotai'
+import { type PrimitiveAtom } from 'jotai/index'
 import { useHydrateAtoms } from 'jotai/utils'
 import { useEffect } from 'react'
 
 import {
+	FONT_SIZE_OFFSET_COOKIE,
 	fontSizeOffsetAtom,
 	hideNonOriginalTextAtom,
 	hideNonOriginalTextDefaultValue,
@@ -19,20 +21,38 @@ import {
 	verseBreaksLineDefaultValue,
 } from '~/app/[bookCode]/_components/TopToolbar/TopToolbar.state'
 
+const useSetupClientState = <T>(
+	atom: PrimitiveAtom<T>,
+	savedValue: T,
+	cookieName: string,
+) => {
+	const [value, setValue] = useAtom(atom)
+
+	useEffect(() => {
+		//eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+		document.cookie = `${cookieName}=${value}`
+	}, [cookieName, value])
+
+	useEffect(() => {
+		setValue(savedValue)
+	}, [savedValue, setValue])
+
+	useHydrateAtoms([[atom, savedValue]])
+}
+
 export const HydrateReaderPageAtoms = ({
 	savedFontSizeOffset,
 }: {
 	savedFontSizeOffset: TFontSizeOffset
 }) => {
-	const setFontSizeOffset = useSetAtom(fontSizeOffsetAtom)
-
-	useEffect(() => {
-		setFontSizeOffset(savedFontSizeOffset)
-	}, [savedFontSizeOffset, setFontSizeOffset])
+	useSetupClientState(
+		fontSizeOffsetAtom,
+		savedFontSizeOffset,
+		FONT_SIZE_OFFSET_COOKIE,
+	)
 
 	useHydrateAtoms([
 		[isPreferencesMenuOpenAtom, isPreferencesMenuOpenDefaultValue],
-		[fontSizeOffsetAtom, savedFontSizeOffset],
 		[leadingAtom, leadingDefaultValue],
 		[verseBreaksLineAtom, verseBreaksLineDefaultValue],
 		[hideNonOriginalTextAtom, hideNonOriginalTextDefaultValue],
--- src/app/[bookCode]/_components/TopToolbar/PreferencesMenu/PreferencesMenu.tsx
+++ src/app/[bookCode]/_components/TopToolbar/PreferencesMenu/PreferencesMenu.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 import { Portal } from '@ark-ui/react'
-import { useAtomValue } from 'jotai'
 import type { PrimitiveAtom } from 'jotai/index'
-import { useEffect } from 'react'
 
 import {
 	Backdrop,
@@ -12,7 +10,6 @@ import {
 } from '~/components'
 
 import {
-	FONT_SIZE_OFFSET_COOKIE,
 	fontSizeOffsetAtom,
 	hideNonOriginalTextAtom,
 	leadingAtom,
@@ -24,58 +21,50 @@ import { PreferencesList } from './PreferencesMenu.styles'
 import { SliderInput } from './SliderInput'
 import { SwitchInput } from './SwitchInput'
 
-export const PreferencesMenu = () => {
-	const fontSizeOffset = useAtomValue(fontSizeOffsetAtom)
-
-	useEffect(() => {
-		document.cookie = `${FONT_SIZE_OFFSET_COOKIE}=${fontSizeOffset}`
-	}, [fontSizeOffset])
-
-	return (
-		<Portal>
-			<Backdrop />
-			<Positioner_OverlayMenu css={{ h: 'calc(100dvh / 3 * 2)' }}>
-				<Container_OverlayMenu
-					css={{
-						h: 'calc(100dvh / 3 * 2)',
-					}}
-				>
-					<Header />
-					<PreferencesList>
-						<SliderInput
-							valueAtom={fontSizeOffsetAtom as PrimitiveAtom<number>}
-							label="Font size"
-							machineProps={{
-								id: 'font-size',
-								name: 'font-size',
-								min: -2,
-								max: 8,
-								step: 1,
-							}}
-						/>
-						<SliderInput
-							valueAtom={leadingAtom as PrimitiveAtom<number>}
-							label="Line gap"
-							machineProps={{
-								id: 'line-gap',
-								name: 'line-gap',
-								min: 1,
-								max: 3,
-								step: 0.25,
-							}}
-						/>
-						<SwitchInput
-							valueAtom={verseBreaksLineAtom}
-							label="Start verse on new line"
-						/>
-						<SwitchInput
-							valueAtom={hideNonOriginalTextAtom}
-							label="Hide non-original text"
-						/>
-						<SwitchInput valueAtom={showRedLettersAtom} label="Red letters" />
-					</PreferencesList>
-				</Container_OverlayMenu>
-			</Positioner_OverlayMenu>
-		</Portal>
-	)
-}
+export const PreferencesMenu = () => (
+	<Portal>
+		<Backdrop />
+		<Positioner_OverlayMenu css={{ h: 'calc(100dvh / 3 * 2)' }}>
+			<Container_OverlayMenu
+				css={{
+					h: 'calc(100dvh / 3 * 2)',
+				}}
+			>
+				<Header />
+				<PreferencesList>
+					<SliderInput
+						valueAtom={fontSizeOffsetAtom as PrimitiveAtom<number>}
+						label="Font size"
+						machineProps={{
+							id: 'font-size',
+							name: 'font-size',
+							min: -2,
+							max: 8,
+							step: 1,
+						}}
+					/>
+					<SliderInput
+						valueAtom={leadingAtom as PrimitiveAtom<number>}
+						label="Line gap"
+						machineProps={{
+							id: 'line-gap',
+							name: 'line-gap',
+							min: 1,
+							max: 3,
+							step: 0.25,
+						}}
+					/>
+					<SwitchInput
+						valueAtom={verseBreaksLineAtom}
+						label="Start verse on new line"
+					/>
+					<SwitchInput
+						valueAtom={hideNonOriginalTextAtom}
+						label="Hide non-original text"
+					/>
+					<SwitchInput valueAtom={showRedLettersAtom} label="Red letters" />
+				</PreferencesList>
+			</Container_OverlayMenu>
+		</Positioner_OverlayMenu>
+	</Portal>
+)
