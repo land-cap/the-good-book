8e559111304f78e086a421f01c27b864e9403ef8
Daniel Filat <filatdaniel98@gmail.com>
2024-01-19 16:42:18 +0200

    falls back to default value if no cookie for preferences state

--- src/app/@topToolbar/_components/TopToolbar/TopToolbar.tsx
+++ src/app/@topToolbar/_components/TopToolbar/TopToolbar.tsx
@@ -10,12 +10,17 @@ import { PreferencesMenuRoot } from './PreferencesMenu'
 import { SetUpPreferencesMenuState } from './SetUpPreferencesMenuState'
 import {
 	FONT_SIZE_OFFSET_COOKIE,
+	fontSizeOffsetDefaultValue,
 	HIDE_NON_ORIGINAL_TEXT_COOKIE,
+	hideNonOriginalTextDefaultValue,
 	LEADING_COOKIE,
+	leadingDefaultValue,
 	SHOW_RED_LETTERS_COOKIE,
+	showRedLettersDefaultValue,
 	type TFontSizeOffset,
 	type TLeading,
 	VERSE_BREAKS_LINE_COOKIE,
+	verseBreaksLineDefaultValue,
 } from './TopToolbar.state'
 
 const Container = wChildren(({ children }) => (
@@ -51,11 +56,19 @@ const Logo = styled('span', {
 	base: { fontWeight: 'bold' },
 })
 
+const getBooleanCookieValue = (
+	cookieValue: string | undefined,
+	fallback: boolean,
+) => (cookieValue ? cookieValue === 'true' : fallback)
+
 export const TopToolbar = () => {
 	const cookieStore = cookies()
 
-	const savedFontSizeOffset = cookieStore.get(FONT_SIZE_OFFSET_COOKIE)?.value
-	const savedLeading = cookieStore.get(LEADING_COOKIE)?.value
+	const savedFontSizeOffset =
+		cookieStore.get(FONT_SIZE_OFFSET_COOKIE)?.value ??
+		fontSizeOffsetDefaultValue
+	const savedLeading =
+		cookieStore.get(LEADING_COOKIE)?.value ?? leadingDefaultValue
 	const savedVerseBreaksLine = cookieStore.get(VERSE_BREAKS_LINE_COOKIE)?.value
 	const savedHideNonOriginalText = cookieStore.get(
 		HIDE_NON_ORIGINAL_TEXT_COOKIE,
@@ -67,9 +80,18 @@ export const TopToolbar = () => {
 			<SetUpPreferencesMenuState
 				savedFontSizeOffset={Number(savedFontSizeOffset) as TFontSizeOffset}
 				savedLeading={Number(savedLeading) as TLeading}
-				savedVerseBreaksLine={savedVerseBreaksLine === 'true'}
-				savedHideNonOriginalText={savedHideNonOriginalText === 'true'}
-				savedShowRedLetters={savedShowRedLetters === 'true'}
+				savedVerseBreaksLine={getBooleanCookieValue(
+					savedVerseBreaksLine,
+					verseBreaksLineDefaultValue,
+				)}
+				savedHideNonOriginalText={getBooleanCookieValue(
+					savedHideNonOriginalText,
+					hideNonOriginalTextDefaultValue,
+				)}
+				savedShowRedLetters={getBooleanCookieValue(
+					savedShowRedLetters,
+					showRedLettersDefaultValue,
+				)}
 			/>
 			<Container>
 				<Logo>The Good Book</Logo>
