41858a987115faa3eb3e3b50272a3a6abfbf9950
Daniel Filat <filatdaniel98@gmail.com>
2024-01-20 12:14:59 +0200

    107 fix toptoolbar route not rendering when deployed (#108)
    * makes @topToolbar page rallel to reader page
    
    * statically generates top toolbar routes
    
    * makes top toolbar route render only default
    
    * tries to persist preferences state in local storage
    
    * Revert "tries to persist preferences state in local storage"
    
    This reverts commit eb590da495d6a281e9bc2c442c79a1a2042354e9.
    
    * debugs preferences state
    
    * persists all preferences state in local storage
    
    * render TopToolbar as regular component in reader route layout
    
    * persists state with cookies and makes reader pages dynamic

--- src/app/[bookCode]/_components/TopToolbar/TopToolbar.state.ts
+++ src/app/[bookCode]/_components/TopToolbar/TopToolbar.state.ts
@@ -1,18 +1,16 @@
 import { atom } from 'jotai'
 
-export type TFontSizeOffset = -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-
-export type TLeading = 1 | 1.25 | 1.5 | 1.75 | 2 | 2.25 | 2.5 | 2.75 | 3
-
 export const isPreferencesMenuOpenDefaultValue = false
 export const isPreferencesMenuOpenAtom = atom(isPreferencesMenuOpenDefaultValue)
 
+export type TFontSizeOffset = -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
 export const FONT_SIZE_OFFSET_COOKIE = 'fontSizeOffset'
 export const fontSizeOffsetDefaultValue = 0
 export const fontSizeOffsetAtom = atom<TFontSizeOffset>(
 	fontSizeOffsetDefaultValue,
 )
 
+export type TLeading = 1 | 1.25 | 1.5 | 1.75 | 2 | 2.25 | 2.5 | 2.75 | 3
 export const LEADING_COOKIE = 'leading'
 export const leadingDefaultValue = 2
 export const leadingAtom = atom<TLeading>(leadingDefaultValue)
