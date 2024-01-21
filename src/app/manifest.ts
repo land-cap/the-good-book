ed60ebd9cbfb69ef42a991345f5a0af080caf852
Daniel Filat <filatdaniel98@gmail.com>
2024-01-21 14:42:02 +0200

    87 fix pwa menubar background on ios (#109)
    * tries to extend html to include safe areas
    
    * Revert "tries to extend html to include safe areas"
    
    This reverts commit f6711a9d537bd127eee029755f63adbc71a746b3.
    
    * sets different theme color for light dark mode

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,25 +1,25 @@
 import { type MetadataRoute } from 'next'
 
-export default function manifest(): MetadataRoute.Manifest {
-	return {
-		description: 'Read the Bible without distractions.',
-		display: 'standalone',
-		icons: [
-			{
-				purpose: 'maskable',
-				sizes: '512x512',
-				src: '/icons/app-icon-512.png',
-				type: 'image/png',
-			},
-			{
-				purpose: 'any',
-				sizes: '512x512',
-				src: '/icons/app-icon-512.png',
-				type: 'image/png',
-			},
-		],
-		name: 'The Good Book',
-		scope: '/',
-		short_name: 'Good Book',
-	}
-}
+const manifest = (): MetadataRoute.Manifest => ({
+	description: 'Read the Bible without distractions.',
+	display: 'standalone',
+	icons: [
+		{
+			purpose: 'maskable',
+			sizes: '512x512',
+			src: '/icons/app-icon-512.png',
+			type: 'image/png',
+		},
+		{
+			purpose: 'any',
+			sizes: '512x512',
+			src: '/icons/app-icon-512.png',
+			type: 'image/png',
+		},
+	],
+	name: 'The Good Book',
+	scope: '/',
+	short_name: 'Good Book',
+})
+
+export default manifest
