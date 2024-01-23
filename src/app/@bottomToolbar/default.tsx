eb39055e5cd451179d7cfb5eb1d110c2cbe10b59
Daniel Filat <filatdaniel98@gmail.com>
2024-01-23 14:01:40 +0200

    renders toolbars as regular components in root layout

--- src/app/@bottomToolbar/default.tsx
+++
@@ -1,8 +0,0 @@
-import { getBookListWithCache } from '~/db'
-import { BottomToolbar } from '~/organisms'
-
-export default async () => {
-	const bookList = await getBookListWithCache()
-
-	return <BottomToolbar bookList={bookList} />
-}
