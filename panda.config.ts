d2410ef8e2b5fd5032c8cfac50627ff86d06af2b
Daniel Filat <filatdaniel98@gmail.com>
2024-01-23 12:22:42 +0200

    fixes BottomToolbar rerendering when params change

--- panda.config.ts
+++ panda.config.ts
@@ -5,21 +5,10 @@ import { pandaPresetGoodBook } from '~/pandaPresetGoodBook'
 
 export default defineConfig({
 	emitPackage: true,
-	// Files to exclude
 	exclude: [],
-	// Where to look for your css declarations
-	include: [
-		'./src/app/**/*.{ts,tsx,js,jsx}',
-		'./src/organisms/**/*.{ts,tsx,js,jsx}',
-		'./src/components/**/*.{ts,tsx,js,jsx}',
-		'./src/_pages/**/*.{ts,tsx,js,jsx}',
-	],
+	include: ['./src/**/*.{ts,tsx,js,jsx}'],
 	jsxFramework: 'react',
-	//jsxFactory: 'all',
-	//jsxStyleProps: 'all',
-	// The output directory for your css system
 	outdir: 'styled-system',
-	// Whether to use css reset
 	preflight: true,
 	presets: ['@pandacss/dev/presets', pandaPreset, pandaPresetGoodBook],
 })
