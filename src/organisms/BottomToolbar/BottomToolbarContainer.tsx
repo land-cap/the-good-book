d1436d118ae17105df210bd7c06eedea278c9d49
Daniel Filat <filatdaniel98@gmail.com>
2024-01-24 15:59:34 +0200

    WIP adds transition for extending and compressing bottom toolbar

--- src/organisms/BottomToolbar/BottomToolbarContainer.tsx
+++ src/organisms/BottomToolbar/BottomToolbarContainer.tsx
@@ -1,28 +1,35 @@
 import { type ReactNode } from 'react'
 import { macrogrid } from 'styled-system/patterns'
 
+import { useShowCompressedBottomToolbar } from '~/organisms/BottomToolbar/useShowCompressedBottomToolbar'
+
 export const BottomToolbarContainer = ({
 	children,
 }: {
 	children: ReactNode
-}) => (
-	<div
-		className={macrogrid({
-			column: 'fullbleed',
-			forceGpu: true,
-			position: 'sticky',
-			bottom: '0',
-			bg: 'bg.canvas',
-			willChange: 'bottom, opacity',
-			transition: 'all',
-			transitionDuration: 'normal',
-			transitionTimingFunction: 'ease-in-out',
-			sm: {
+}) => {
+	const showCompressedToolbar = useShowCompressedBottomToolbar()
+
+	return (
+		<div
+			className={macrogrid({
+				column: 'fullbleed',
+				forceGpu: true,
+				position: 'sticky',
 				bottom: '0',
-				opacity: '1',
-			},
-		})}
-	>
-		{children}
-	</div>
-)
+				h: showCompressedToolbar ? '14' : '8',
+				bg: 'bg.canvas',
+				willChange: 'bottom, opacity',
+				transition: 'all',
+				transitionDuration: 'normal',
+				transitionTimingFunction: 'ease-in-out',
+				sm: {
+					bottom: '0',
+					opacity: '1',
+				},
+			})}
+		>
+			{children}
+		</div>
+	)
+}
