cf2b54401d2c5261bb03eaf58f9653df4dddca6a
Daniel Filat <filatdaniel98@gmail.com>
2024-01-24 16:07:58 +0200

    always shows bottom toolbar and prevents it from comporessing on scroll

--- src/organisms/BottomToolbar/BottomToolbar.tsx
+++ src/organisms/BottomToolbar/BottomToolbar.tsx
@@ -1,34 +1,85 @@
 'use client'
 
-import { useAtomValue } from 'jotai'
-import { css } from 'styled-system/css'
+import { useParams } from 'next/navigation'
+import { flex, subgrid } from 'styled-system/patterns'
 
-import { Separator } from '~/components'
-import { type TBook } from '~/db'
-import { forceShowControlsAtom } from '~/global.state'
-import { BottomToolbarCompressed } from '~/organisms/BottomToolbarScrolled'
+import type { TReaderPageParams } from '~/_pages/ReaderPage/ReaderPage.types'
+import type { TBook } from '~/db'
 
 import { BottomToolbarContainer } from './BottomToolbarContainer'
-import { BottomToolbarExtended } from './BottomToolbarExtended'
-import { useShowCompressedBottomToolbar } from './useShowCompressedBottomToolbar'
+import { ChapterPickerMenu } from './ChapterPickerMenu'
+import { ReaderNavButton } from './ReaderNavButton'
 
 export const BottomToolbar = ({ bookList }: { bookList: TBook[] }) => {
-	const forceShowControls = useAtomValue(forceShowControlsAtom)
+	const { bookCode, chapter: _chapter } = useParams<TReaderPageParams>()
 
-	const showCompressedToolbar = useShowCompressedBottomToolbar()
+	const chapter = Number(_chapter)
 
-	const showCompressedVersion = showCompressedToolbar
+	const currBook = bookList.find((book) => book.code === bookCode)
 
-	console.log(forceShowControls)
+	if (!currBook) {
+		throw new Error('No book data')
+	}
 
+	const currBookIndex = bookList.findIndex(
+		(book) => book.book_name?.name === currBook.book_name?.name,
+	)
+
+	const currBookChapterCount = bookList[currBookIndex]?.chapter_count
+
+	const prevBook = bookList[currBookIndex - 1]
+
+	const prevBookCode = prevBook?.code
+
+	const prevBookChapterCount = prevBook?.chapter_count
+
+	const nextBookCode = bookList[currBookIndex + 1]?.code
+
+	const prevChapterHref =
+		chapter === 1
+			? `/${prevBookCode}/${prevBookChapterCount}`
+			: `/${bookCode}/${chapter - 1}`
+
+	const nextChapterHref =
+		chapter === currBookChapterCount
+			? `/${nextBookCode}/1`
+			: `/${bookCode}/${chapter + 1}`
+
+	const isFirstChapterInBible = currBookIndex === 0 && chapter === 1
+
+	const isLastChapterInBible =
+		currBookIndex === bookList.length - 1 && chapter === currBookChapterCount
 	return (
 		<BottomToolbarContainer>
-			<Separator className={css({ column: 'content' })} />
-			{showCompressedVersion ? (
-				<BottomToolbarExtended bookList={bookList} />
-			) : (
-				<BottomToolbarCompressed />
-			)}
+			<div
+				className={subgrid({
+					column: 'content',
+					pb: 'safe_area_bottom',
+				})}
+			>
+				<div
+					className={flex({
+						alignItems: 'center',
+						h: '14',
+					})}
+				>
+					<ReaderNavButton
+						href={prevChapterHref}
+						direction="left"
+						isDisabled={isFirstChapterInBible}
+					/>
+					<ChapterPickerMenu
+						currChapter={chapter}
+						currBook={currBook}
+						bookList={bookList}
+					/>
+					<ReaderNavButton
+						href={nextChapterHref}
+						direction="right"
+						isDisabled={isLastChapterInBible}
+					/>
+				</div>
+			</div>
 		</BottomToolbarContainer>
 	)
 }
--- src/organisms/BottomToolbar/BottomToolbarContainer.tsx
+++ src/organisms/BottomToolbar/BottomToolbarContainer.tsx
@@ -1,35 +1,28 @@
 import { type ReactNode } from 'react'
+import { css } from 'styled-system/css'
 import { macrogrid } from 'styled-system/patterns'
 
-import { useShowCompressedBottomToolbar } from '~/organisms/BottomToolbar/useShowCompressedBottomToolbar'
+import { Separator } from '~/components'
 
 export const BottomToolbarContainer = ({
 	children,
 }: {
 	children: ReactNode
-}) => {
-	const showCompressedToolbar = useShowCompressedBottomToolbar()
-
-	return (
-		<div
-			className={macrogrid({
-				column: 'fullbleed',
-				forceGpu: true,
-				position: 'sticky',
-				bottom: '0',
-				h: showCompressedToolbar ? '14' : '8',
-				bg: 'bg.canvas',
-				willChange: 'bottom, opacity',
-				transition: 'all',
-				transitionDuration: 'normal',
-				transitionTimingFunction: 'ease-in-out',
-				sm: {
-					bottom: '0',
-					opacity: '1',
-				},
-			})}
-		>
-			{children}
-		</div>
-	)
-}
+}) => (
+	<nav
+		className={macrogrid({
+			column: 'fullbleed',
+			forceGpu: true,
+			position: 'sticky',
+			bottom: '0',
+			bg: 'bg.canvas',
+			willChange: 'bottom, opacity',
+			transition: 'all',
+			transitionDuration: 'normal',
+			transitionTimingFunction: 'ease-in-out',
+		})}
+	>
+		<Separator className={css({ column: 'content' })} />
+		{children}
+	</nav>
+)
