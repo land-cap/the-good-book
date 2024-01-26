0f5c16b16dbe565957a79f64d9ef8ddb30377de3
Daniel Filat <filatdaniel98@gmail.com>
2024-01-26 09:33:00 +0200

    tweaks active state of solid button and preferences list spacing

--- src/pandaPresetGoodBook/semanticTokens/colors.semanticTokens/bg.semanticTokens.ts
+++ src/pandaPresetGoodBook/semanticTokens/colors.semanticTokens/bg.semanticTokens.ts
@@ -7,16 +7,22 @@ export const bg = defineSemanticTokens.colors({
 			base: '{colors.white}',
 		},
 	},
+	subtle: {
+		value: {
+			_osDark: '{colors.neutral.800}',
+			base: '{colors.neutral.100}',
+		},
+	},
 	muted: {
 		value: {
 			_osDark: '{colors.neutral.700}',
 			base: '{colors.neutral.200}',
 		},
 	},
-	subtle: {
+	more_muted: {
 		value: {
-			_osDark: '{colors.neutral.800}',
-			base: '{colors.neutral.100}',
+			_osDark: '{colors.neutral.600}',
+			base: '{colors.neutral.300}',
 		},
 	},
 	inverted: {
