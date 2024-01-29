5e70a0a23f0c0027308761ae4716d222ee83da8a
Daniel Filat <filatdaniel98@gmail.com>
2024-01-29 16:55:28 +0200

    adds styles to popover

--- src/_pages/ReaderPage/chapterContent/renderChapterContent/CrossReferencePopover.tsx
+++ src/_pages/ReaderPage/chapterContent/renderChapterContent/CrossReferencePopover.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Popover, Portal } from '@ark-ui/react'
+import { useState } from 'react'
 import { css } from 'styled-system/css'
 import { useIsClient } from 'usehooks-ts'
 
@@ -10,8 +11,19 @@ export const CrossReferencePopover = ({
 	references: string
 }) => {
 	const isClient = useIsClient()
+
+	const [isOpen, setIsOpen] = useState(false)
+
 	return (
-		<Popover.Root data-component="CrossReference" portalled positioning={{}}>
+		<Popover.Root
+			data-component="CrossReference"
+			portalled
+			onOpenChange={({ open }) => setIsOpen(open)}
+			positioning={{
+				gutter: 0,
+				shift: 0,
+			}}
+		>
 			<Popover.Trigger>
 				&nbsp;
 				<span
@@ -21,7 +33,10 @@ export const CrossReferencePopover = ({
 						p: '1',
 						fontFamily: 'sans',
 						fontWeight: '1000',
-						color: 'fg.faded',
+						color: isOpen ? 'fg' : 'fg.faded',
+						transition: 'colors',
+						transitionDuration: 'fast',
+						transitionTimingFunction: 'ease-out',
 					})}
 				>
 					&dagger;
@@ -30,9 +45,19 @@ export const CrossReferencePopover = ({
 			{isClient ? (
 				<Portal>
 					<Popover.Positioner>
-						<Popover.Content>
-							<Popover.Title>Cross References</Popover.Title>
-							<Popover.Description>{references}</Popover.Description>
+						<Popover.Content
+							className={css({
+								bg: 'bg.canvas',
+								p: '4',
+								borderWidth: '1px',
+								borderColor: 'fg',
+							})}
+						>
+							<Popover.Description
+								className={css({ fontStyle: '1rem', fontWeight: 'bold' })}
+							>
+								{references}
+							</Popover.Description>
 						</Popover.Content>
 					</Popover.Positioner>
 				</Portal>
