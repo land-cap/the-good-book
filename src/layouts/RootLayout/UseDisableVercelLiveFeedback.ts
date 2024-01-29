be10567c247ee81cfd32570a6ea1adcf5a240b6a
Daniel Filat <filatdaniel98@gmail.com>
2024-01-29 13:12:44 +0200

    Revert "force disables vercel live feedback"
    This reverts commit a3a3c03300b788075bc85a524f7908f548083850.

--- src/layouts/RootLayout/UseDisableVercelLiveFeedback.ts
+++
@@ -1,7 +0,0 @@
-import { useEffect } from 'react'
-
-export const UseDisableVercelLiveFeedback = () => {
-	useEffect(() => document.querySelector('vercel-live-feedback')?.remove(), [])
-
-	return null
-}
