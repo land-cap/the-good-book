aa1607177b4ae8157305a43c47dce51bc4cc5e32
Daniel Filat <filatdaniel98@gmail.com>
2024-02-21 11:13:43 +0200

    improves FAB button styles and adds icon

--- src/organisms/BottomToolbar/ReturnFromReference.tsx
+++ src/organisms/BottomToolbar/ReturnFromReference.tsx
@@ -1,9 +1,14 @@
 import { css, cx } from 'styled-system/css'
 import { button } from 'styled-system/recipes'
 
+import { Icon } from '~/components'
+
 const buttonCls = cx(
 	button({ visual: 'solid', size: 'small', weight: 'regular' }),
 	css({
+		display: 'inline-flex',
+		gap: '1.5',
+		alignItems: 'center',
 		pos: 'absolute',
 		top: '-4',
 		right: '0',
@@ -11,6 +16,21 @@ const buttonCls = cx(
 	}),
 )
 
+const iconCls = css({
+	'--wght': '325',
+	ml: '-0.5',
+	_active: { color: 'fg' },
+	_canHover: { _hover: { color: 'fg' } },
+	transition: 'colors',
+	transitionDuration: 'fast',
+	transitionTimingFunction: 'ease-out',
+})
+
 export const ReturnFromReferenceFAB = () => {
-	return <button className={buttonCls}>Gen. 12</button>
+	return (
+		<button className={buttonCls}>
+			<Icon name="undo" size={5} className={iconCls} />
+			Gen. 12
+		</button>
+	)
 }
