c6866d2475ea35963a288ec1030bb83eab391fe5
Daniel Filat <filatdaniel98@gmail.com>
2024-02-23 17:04:07 +0200

    WIP uses custom font in og image

--- src/app/[bookCode]/[chapter]/opengraph-image.tsx
+++ src/app/[bookCode]/[chapter]/opengraph-image.tsx
@@ -1,13 +1,40 @@
 import { ImageResponse } from 'next/og'
+import { type CSSProperties } from 'react'
 
 import { type TReaderPageParams } from '~/_pages/ReaderPage/ReaderPage.types'
 import { getBookNameByCode } from '~/organisms/VerseDetailsMenu/getBookNameByCode'
+
+export const runtime = 'nodejs'
 
 export const size = {
 	width: 1200,
 	height: 630,
 }
+
 export const contentType = 'image/png'
+
+const containerStyles: CSSProperties = {
+	background: 'white',
+	width: '100%',
+	height: '100%',
+	display: 'flex',
+	flexFlow: 'column nowrap',
+	alignItems: 'flex-start',
+	justifyContent: 'space-between',
+	padding: 64,
+}
+
+const titleStyles: CSSProperties = {
+	fontSize: 128,
+	fontWeight: 600,
+	lineHeight: 1,
+}
+
+const subTitleStyles: CSSProperties = {
+	fontSize: 48,
+	fontWeight: 600,
+	lineHeight: 1,
+}
 
 export default async function Image({
 	params: { bookCode, chapter },
@@ -18,18 +45,11 @@ export default async function Image({
 
 	return new ImageResponse(
 		(
-			<div
-				style={{
-					fontSize: 32,
-					background: 'white',
-					width: '100%',
-					height: '100%',
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'center',
-				}}
-			>
-				{bookName} {chapter}
+			<div style={containerStyles}>
+				<p style={titleStyles}>
+					{bookName} {chapter}
+				</p>
+				<p style={subTitleStyles}>The Good Book</p>
 			</div>
 		),
 		{
