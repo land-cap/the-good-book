5772a6efe8da0f667eb9d1522c61fa77edf16acb
Daniel Filat <filatdaniel98@gmail.com>
2024-02-24 15:58:07 +0200

    Lan 139 set up open graph meta tags for reader page (#133)
    * generates opengraph image for reader page
    
    * WIP uses custom font in og image
    
    * improves spacing
    
    * WIP loads custom font in og image generator
    
    * creates dedicated route handler for open graph image
    
    * sets up og route for reader page to redirect to og route handler
    
    * adds minor ui tweaks
    
    * makes dark theme lighter
    
    * moves preferences menu trigger to top toolbar
    
    * hides reference fab on navigation
    
    * fix: edge cases in reference processing
    
    * fix: exodus references

---
+++ src/app/og/route.tsx
@@ -0,0 +1,86 @@
+import { ImageResponse } from 'next/og'
+import { type NextRequest } from 'next/server'
+import { type CSSProperties } from 'react'
+
+export const runtime = 'edge'
+
+const containerStyles: CSSProperties = {
+	background: 'white',
+	width: '100%',
+	height: '100%',
+	display: 'flex',
+	flexFlow: 'column nowrap',
+	alignItems: 'flex-start',
+	justifyContent: 'space-between',
+	padding: 96,
+}
+
+const titleStyles: CSSProperties = {
+	fontFamily: 'sans-bold',
+	fontSize: 128,
+	fontWeight: 600,
+	lineHeight: 1.1,
+}
+
+const subTitleStyles: CSSProperties = {
+	fontFamily: 'sans-bold',
+	fontSize: 48,
+	fontWeight: 600,
+	lineHeight: 1.1,
+}
+
+export async function GET(request: NextRequest) {
+	const { searchParams } = new URL(request.url)
+	const bookName = searchParams.get('book-name')
+	const chapter = searchParams.get('chapter')
+
+	const fontSansBoldData = await fetch(
+		new URL('../../../assets/fonts/Geist-UltraBlack.otf', import.meta.url),
+	).then((res) => res.arrayBuffer())
+
+	return new ImageResponse(
+		bookName ? (
+			<div style={containerStyles}>
+				<p style={titleStyles}>
+					{bookName} {chapter}
+				</p>
+				<p style={subTitleStyles}>The Good Book</p>
+			</div>
+		) : (
+			<div
+				style={{
+					background: 'white',
+					width: '100%',
+					height: '100%',
+					display: 'flex',
+					flexFlow: 'column nowrap',
+					alignItems: 'center',
+					justifyContent: 'center',
+					padding: 96,
+				}}
+			>
+				<p
+					style={{
+						fontFamily: 'sans-bold',
+						fontSize: 96,
+						fontWeight: 600,
+						lineHeight: 1.1,
+					}}
+				>
+					The Good Book
+				</p>
+			</div>
+		),
+		{
+			width: 1200,
+			height: 630,
+			fonts: [
+				{
+					name: 'sans-bold',
+					data: fontSansBoldData,
+					style: 'normal',
+				},
+			],
+		},
+	)
+}
--- src/pandaPresetGoodBook/semanticTokens/colors.semanticTokens/bg.semanticTokens.ts
+++ src/pandaPresetGoodBook/semanticTokens/colors.semanticTokens/bg.semanticTokens.ts
@@ -4,25 +4,25 @@ export const bg = defineSemanticTokens.colors({
 	canvas: {
 		value: {
 			base: '{colors.white}',
-			_osDark: '{colors.neutral.900}',
+			_osDark: '{colors.neutral.800}',
 		},
 	},
 	subtle: {
 		value: {
 			base: '{colors.neutral.100}',
-			_osDark: '{colors.neutral.800}',
+			_osDark: '{colors.neutral.700}',
 		},
 	},
 	muted: {
 		value: {
 			base: '{colors.neutral.200}',
-			_osDark: '{colors.neutral.700}',
+			_osDark: '{colors.neutral.600}',
 		},
 	},
 	more_muted: {
 		value: {
 			base: '{colors.neutral.300}',
-			_osDark: '{colors.neutral.600}',
+			_osDark: '{colors.neutral.500}',
 		},
 	},
 	inverted: {
