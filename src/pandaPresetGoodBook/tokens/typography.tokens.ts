61b5673fa4be81b20a9c36bf6ef38cfed351b171
Daniel Filat <filatdaniel98@gmail.com>
2024-02-28 15:17:47 +0200

    sets up serif font

--- src/pandaPresetGoodBook/tokens/typography.tokens.ts
+++ src/pandaPresetGoodBook/tokens/typography.tokens.ts
@@ -1,8 +1,9 @@
 import { defineTokens } from '@pandacss/dev'
 
 export const fonts = defineTokens.fonts({
-	mono: { value: 'var(--font-mono)' },
 	sans: { value: 'var(--font-sans)' },
+	mono: { value: 'var(--font-mono)' },
+	serif: { value: 'var(--font-serif)' },
 })
 
 export const fontWeights = defineTokens.fontWeights({
