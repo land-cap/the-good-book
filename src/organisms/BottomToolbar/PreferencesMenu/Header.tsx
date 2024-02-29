bd3e3be299dec5230f100f25b9e450db041bf0fb
Daniel Filat <filatdaniel98@gmail.com>
2024-02-29 13:53:10 +0200

    fix: scroll not working inside preferences menu

--- src/organisms/BottomToolbar/PreferencesMenu/Header.tsx
+++ src/organisms/BottomToolbar/PreferencesMenu/Header.tsx
@@ -1,18 +1,25 @@
 import { Dialog } from '@ark-ui/react'
 import { css } from 'styled-system/css'
+import { Macrogrid } from 'styled-system/jsx'
 import { hstack } from 'styled-system/patterns'
 import { button } from 'styled-system/recipes'
 
 import { Icon, Separator } from '~/components'
 
 export const Header = () => (
-	<div className={css({ column: 'content' })}>
-		<div className={hstack({ h: '14', justify: 'space-between' })}>
+	<Macrogrid>
+		<div
+			className={hstack({
+				justify: 'space-between',
+				column: 'content',
+				h: '14',
+			})}
+		>
 			<h2 className={css({ fontWeight: 'bold' })}>Preferences</h2>
 			<Dialog.CloseTrigger className={button({ icon: true })}>
 				<Icon size={6} name="close" />
 			</Dialog.CloseTrigger>
 		</div>
 		<Separator css={{ column: 'content' }} />
-	</div>
+	</Macrogrid>
 )
