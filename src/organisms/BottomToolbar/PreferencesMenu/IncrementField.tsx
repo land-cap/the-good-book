2942493be24393c5f02cf649d77e7006f8d40edf
Daniel Filat <filatdaniel98@gmail.com>
2024-03-04 17:40:19 +0200

    improves increment field

--- src/organisms/BottomToolbar/PreferencesMenu/IncrementField.tsx
+++ src/organisms/BottomToolbar/PreferencesMenu/IncrementField.tsx
@@ -6,22 +6,19 @@ import { Icon } from '~/components'
 import { useRangeInput } from '~/hooks'
 
 const buttonCls = cx(
-	button({ icon: true, visual: 'solid' }),
+	button({ icon: true, visual: 'solid', size: 'md' }),
 	css({
-		w: '12',
-		h: '10',
+		flexGrow: 1,
 	}),
 )
 
 export const IncrementField = <T,>({
-	label,
 	range,
 	value,
 	onChange,
 	decreaseIcon,
 	increaseIcon,
 }: {
-	label: string
 	range: T[]
 	value: T
 	onChange: (values: T) => void
@@ -31,32 +28,29 @@ export const IncrementField = <T,>({
 	const { decrement, increment } = useRangeInput(range, value, onChange)
 
 	return (
-		<div className={hstack({ justify: 'space-between', h: '10' })}>
-			<label>{label}</label>
-			<div className={hstack({ gap: '0' })}>
-				<div
-					className={buttonCls}
-					onClick={decrement}
-					role="button"
-					aria-disabled={value === range[0]}
-				>
-					<Icon size={5} weight={500} name={decreaseIcon} />
-				</div>
-				<div
-					className={css({
-						h: '10',
-						w: '1px',
-						bg: 'border.onBgMuted',
-					})}
-				/>
-				<div
-					className={buttonCls}
-					onClick={increment}
-					role="button"
-					aria-disabled={value === range[range.length - 1]}
-				>
-					<Icon size={5} weight={500} name={increaseIcon} />
-				</div>
+		<div className={hstack({ gap: '0' })}>
+			<div
+				className={buttonCls}
+				onClick={decrement}
+				role="button"
+				aria-disabled={value === range[0]}
+			>
+				<Icon size={5} weight={500} name={decreaseIcon} />
+			</div>
+			<div
+				className={css({
+					h: '10',
+					w: '1px',
+					bg: 'border.onBgMuted',
+				})}
+			/>
+			<div
+				className={buttonCls}
+				onClick={increment}
+				role="button"
+				aria-disabled={value === range[range.length - 1]}
+			>
+				<Icon size={5} weight={500} name={increaseIcon} />
 			</div>
 		</div>
 	)
