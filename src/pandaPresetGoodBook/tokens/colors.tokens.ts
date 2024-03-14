7cb568408f869afe840e038fbdc0ac8be8557952
Daniel Filat <filatdaniel98@gmail.com>
2024-03-14 12:10:32 +0200

    adds sepia color tokens

--- src/pandaPresetGoodBook/tokens/colors.tokens.ts
+++ src/pandaPresetGoodBook/tokens/colors.tokens.ts
@@ -2,5 +2,17 @@ import { defineTokens } from '@pandacss/dev'
 
 export const colors = defineTokens.colors({
 	black: { value: '{colors.neutral.900}' },
+	sepia: {
+		50: { value: '#F6EEE7' },
+		100: { value: '#F0E4D9' },
+		200: { value: '#EBDACA' },
+		300: { value: '#E4CDB7' },
+		400: { value: '#CEA37E' },
+		500: { value: '#AD7F58' },
+		600: { value: '#815E46' },
+		700: { value: '#755641' },
+		800: { value: '#6a4f3d' },
+		900: { value: '#5f483a' },
+	},
 	white: { value: 'white' },
 })
