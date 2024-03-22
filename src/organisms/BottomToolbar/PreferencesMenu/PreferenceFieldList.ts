c1145a1f30b5fe5c73b60fbe2e3f24fcc80784b2
Daniel Filat <filatdaniel98@gmail.com>
2024-03-22 10:34:35 +0200

    fix: scroll not working in preferences menu

--- src/organisms/BottomToolbar/PreferencesMenu/PreferenceFieldList.ts
+++ src/organisms/BottomToolbar/PreferencesMenu/PreferenceFieldList.ts
@@ -1,7 +1,7 @@
 import { styled } from 'styled-system/jsx'
 import { flex } from 'styled-system/patterns'
 
-export const SwitchFieldList = styled('form', {
+export const PreferenceFieldList = styled('form', {
 	base: flex.raw({
 		direction: 'column',
 		gap: '8',
