c462183f41a2efb52a447bdcc37aa2c730c3709b
Daniel Filat <filatdaniel98@gmail.com>
2024-08-24 10:00:08 +0300

    fix: restoring prev session chapter breaking server render

--- src/layouts/ReaderLayout/UseRestorePrevSessionChapter.ts
+++ src/layouts/ReaderLayout/UseRestorePrevSessionChapter.ts
@@ -1,18 +1,21 @@
 'use client'
 
-import { useLocalStorage } from '@mantine/hooks'
 import { useSelectedLayoutSegments } from 'next/navigation'
+import { useLocalStorage } from 'usehooks-ts'
+
+import { isInBrowser } from '~/helpers.client'
 
 const READER_PATHNAME_LS_KEY = 'reader-pathname'
 
 export const UseRestorePrevSessionChapter = () => {
-	const [readerPathname, setReaderPathname] = useLocalStorage({
-		key: READER_PATHNAME_LS_KEY,
-	})
+	const [readerPathname, setReaderPathname] = useLocalStorage(
+		READER_PATHNAME_LS_KEY,
+		'',
+	)
 
 	const readerPathSegments = useSelectedLayoutSegments()
 
-	if (readerPathname !== readerPathSegments.join('/')) {
+	if (isInBrowser && readerPathname !== readerPathSegments.join('/')) {
 		setReaderPathname(readerPathSegments.join('/'))
 	}
 
