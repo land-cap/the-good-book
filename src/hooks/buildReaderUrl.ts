f57a9efb54c244b04f6c5d59f2110794c7378587
Daniel Filat <filatdaniel98@gmail.com>
2024-08-31 14:27:30 +0300

    redirects to reader root path if URL is not a valid reader sub-path

--- src/hooks/buildReaderUrl.ts
+++ src/hooks/buildReaderUrl.ts
@@ -1,6 +1,6 @@
 import { VERSE_RANGE_SEARCH_PARAM } from '~/layouts/ReaderLayout/readerLayout.constants'
 
-const readerLayoutPathname = '/read/'
+const readerLayoutPathname = '/read'
 
 export const buildReaderUrl = ({
 	bookCode,
@@ -10,14 +10,16 @@ export const buildReaderUrl = ({
 	bookCode?: string
 	chapter?: number
 	verseRange?: string | number
-}) => {
+} = {}) => {
 	if (!bookCode) {
 		return readerLayoutPathname
 	}
+
 	if (!chapter) {
-		return `${readerLayoutPathname}${bookCode}`
+		return `${readerLayoutPathname}/${bookCode}`
 	}
-	return `${readerLayoutPathname}${bookCode}/${chapter}${
+
+	return `${readerLayoutPathname}/${bookCode}/${chapter}${
 		verseRange ? `?${VERSE_RANGE_SEARCH_PARAM}=${verseRange}` : ''
 	}`
 }
