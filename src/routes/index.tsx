751aa4bb021c300b5be74b1171b58cb8c023c631
Daniel Filat <filatdaniel98@gmail.com>
2025-07-13 17:23:56 +0300

    removes unneeded example code

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
+
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!!!</h3>
+    <div >
+      I am home page.
     </div>
   )
 }
