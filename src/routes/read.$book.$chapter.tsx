dde2e2954ff8dbea0845e6b776b57b942bd7fb18
Daniel Filat <filatdaniel98@gmail.com>
2025-07-23 22:01:25 +0300

    chore: improves code

--- src/routes/read.$book.$chapter.tsx
+++ src/routes/read.$book.$chapter.tsx
@@ -1,22 +1,21 @@
 import { createFileRoute, Link, notFound } from '@tanstack/react-router'
 import { isOnServer } from '~/utils/shared'
 
-// Helper to load chapter data
 async function loadChapter({
-	book,
-	chapter,
-}: {
+														 book,
+														 chapter,
+													 }: {
 	book: string
 	chapter: string
 }) {
-	// Only use Prisma on the server
 	if (isOnServer()) {
-		// Import here to avoid bundling Prisma in client
 		const { dbClient } = await import('~/db/dbClient')
 		const chapterInt = parseInt(chapter, 10)
 		const bookRecord = await dbClient.book.findUnique({ where: { code: book } })
-		if (!bookRecord) return null
-		return await dbClient.chapter.findFirst({
+		if (!bookRecord) {
+			return null
+		}
+		return dbClient.chapter.findFirst({
 			where: { book_id: bookRecord.book_id, chapter: chapterInt },
 		})
 	} else {
@@ -29,7 +28,9 @@ export const Route = createFileRoute('/read/$book/$chapter')({
 	component: ReadChapter,
 	loader: async ({ params }) => {
 		const data = await loadChapter(params)
-		if (!data) throw notFound()
+		if (!data) {
+			throw notFound()
+		}
 		return data
 	},
 })
@@ -41,9 +42,11 @@ function ReadChapter() {
 
 	return (
 		<div>
-			<Link to={'/read/$book/$chapter'} params={{ book: params.book, chapter: `${parseInt(params.chapter) -1}` }}>Previous chapter</Link>
-			<Link to={'/read/$book/$chapter'} params={{ book: params.book, chapter: `${parseInt(params.chapter) +1}` }}>Next chapter</Link>
-			<br/>
+			<Link to={'/read/$book/$chapter'} params={{ book: params.book, chapter: `${parseInt(params.chapter) - 1}` }}>Previous
+				chapter</Link>
+			<Link to={'/read/$book/$chapter'} params={{ book: params.book, chapter: `${parseInt(params.chapter) + 1}` }}>Next
+				chapter</Link>
+			<br />
 			<div dangerouslySetInnerHTML={{ __html: chapterData.content }} />
 		</div>
 	)
