a6d2d5cc4a569fd5cf3c8b8f2d5dff65c25ba7ac
Daniel Filat <filatdaniel98@gmail.com>
2025-07-24 21:26:45 +0300

    feat: adds RootLayout

---
+++ src/features/root-layout/_/GlobalBackdrop.tsx
@@ -0,0 +1,31 @@
+import { useAtomValue } from 'jotai'
+import { AnimatePresence, motion } from 'motion/react'
+import { css } from 'styled-system/css'
+
+import { showBackdropAtom } from './state'
+
+export const GlobalBackdrop = () => {
+   const show = useAtomValue(showBackdropAtom)
+
+   return (
+      <AnimatePresence>
+         {show && (
+            <motion.div
+               initial={{ opacity: 0 }}
+               animate={{ opacity: 0.5 }}
+               exit={{
+                  opacity: 0,
+                  transition: { duration: 0.15, ease: 'easeOut' },
+               }}
+               transition={{ duration: 0.3, ease: 'easeOut' }}
+               className={css({
+                  zIndex: 1,
+                  pos: 'fixed',
+                  inset: 0,
+                  bg: 'bg.canvas',
+               })}
+            />
+         )}
+      </AnimatePresence>
+   )
+}
--- src/features/root-layout/_/RootLayout.tsx
+++ src/features/root-layout/_/RootLayout.tsx
@@ -1,44 +1,42 @@
-import { HeadContent, Scripts } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-import * as React from 'react'
-import { ReactNode } from 'react'
-import { useEffect } from 'react'
-import { registerSW } from 'virtual:pwa-register'
+import { Provider } from 'jotai'
+import { type ReactNode } from 'react'
+import { macrogrid } from 'styled-system/patterns'
 
-const UseServiceWorkerRegister = () => {
-	useEffect(() => {
-		void (async () => {
-			if ('serviceWorker' in navigator) {
-				registerSW({ immediate: true })
-				await navigator.serviceWorker.ready
-				import('~/utils/shared')
-					.then((mod) => mod.getBibleData())
-					.catch((error) => {
-						console.error('Service worker registration failed:', error)
-					})
-			}
-		})()
-	}, [])
+import { SafeAreaBottom } from '~/ui/shared'
 
-	return null
-}
-
+import { GlobalBackdrop } from './GlobalBackdrop'
 
 export const RootLayout = ({ children }: { children: ReactNode }) => {
-	return (
-		// TODO: pass correct lang attribute when implementing i18n
-		// eslint-disable-next-line jsx-a11y/html-has-lang
-		<html>
-		{/* TODO: add title and all required meta tags */}
-		<head>
-			<HeadContent />
-		</head>
-		<body>
-		{children}
-		<UseServiceWorkerRegister />
-		<TanStackRouterDevtools position="bottom-right" />
-		<Scripts />
-		</body>
-		</html>
-	)
+   return (
+      <Provider>
+         {/* TODO: pass current locale when i18n is implemented */}
+         {/* eslint-disable-next-line jsx-a11y/html-has-lang */}
+         <html>
+            <head>
+               <link
+                  rel="stylesheet"
+                  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=arrow_back,arrow_drop_down,arrow_forward,close,custom_typography,density_medium,density_small,info,page_info,text_decrease,text_increase,undo"
+               />
+            </head>
+            <body
+               // TODO: set current theme
+               // data-theme={savedTheme}
+               className={macrogrid({
+                  gridTemplateRows: 'min-content 1fr min-content',
+                  minH: '100dvh',
+                  overscrollBehavior: 'contain',
+                  pb: '14',
+                  fontSize: 'sm',
+                  color: 'fg',
+                  background: 'bg.canvas',
+                  sm: { fontSize: 'md' },
+               })}
+            >
+               {children}
+               <SafeAreaBottom css={{ column: 'content' }} />
+               <GlobalBackdrop />
+            </body>
+         </html>
+      </Provider>
+   )
 }
