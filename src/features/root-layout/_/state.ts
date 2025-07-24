9dc07fbd9c2a3f83a741e0f2287201049ae1534d
Daniel Filat <filatdaniel98@gmail.com>
2025-07-24 21:26:45 +0300

    feat: adds RootLayout

---
+++ src/features/root-layout/_/state.ts
@@ -0,0 +1,5 @@
+import { atom } from 'jotai'
+
+export const isScrollLockedAtom = atom(false)
+
+export const showBackdropAtom = atom(false)
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,43 +6,41 @@ import { DefaultCatchBoundary, NotFound } from '@/error-ui/shared'
 import { RootLayout } from '@/root-layout/shared'
 import appCss from '~/styles/global.css?url'
 
-
 export const Route = createRootRoute({
-	head: () => ({
-		meta: [
-			{
-				charSet: 'utf-8',
-			},
-			{
-				name: 'viewport',
-				content: 'width=device-width, initial-scale=1',
-			},
-		],
-		links: [
-			{ rel: 'stylesheet', href: appCss },
-			{
-				rel: 'apple-touch-icon',
-				sizes: '180x180',
-				href: '/apple-touch-icon.png',
-			},
-			{
-				rel: 'icon',
-				type: 'image/png',
-				sizes: '32x32',
-				href: '/favicon-32x32.png',
-			},
-			{
-				rel: 'icon',
-				type: 'image/png',
-				sizes: '16x16',
-				href: '/favicon-16x16.png',
-			},
-			{ rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
-			{ rel: 'icon', href: '/favicon.ico' },
-		],
-	}),
-	errorComponent: DefaultCatchBoundary,
-	notFoundComponent: () => <NotFound />,
-	shellComponent: RootLayout,
+   head: () => ({
+      meta: [
+         {
+            charSet: 'utf-8',
+         },
+         {
+            name: 'viewport',
+            content: 'width=device-width, initial-scale=1',
+         },
+      ],
+      links: [
+         { rel: 'stylesheet', href: appCss },
+         {
+            rel: 'apple-touch-icon',
+            sizes: '180x180',
+            href: '/apple-touch-icon.png',
+         },
+         {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            href: '/favicon-32x32.png',
+         },
+         {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            href: '/favicon-16x16.png',
+         },
+         { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+         { rel: 'icon', href: '/favicon.ico' },
+      ],
+   }),
+   errorComponent: DefaultCatchBoundary,
+   notFoundComponent: () => <NotFound />,
+   shellComponent: RootLayout,
 })
-
