4be65badbfe80d8f687659076c2686802fddb776
Daniel Filat <filatdaniel98@gmail.com>
2025-07-24 16:31:36 +0300

    refactor: moves ReaderView to read feature

---
+++ src/features/read/_/ReaderView.tsx
@@ -0,0 +1,45 @@
+import { css, cx } from 'styled-system/css'
+import { hstack } from 'styled-system/patterns'
+import { getRouteApi, Link } from '@tanstack/react-router'
+import { button } from 'styled-system/recipes'
+
+export const ReaderView = () => {
+	const params = getRouteApi('/read/$book/$chapter').useParams()
+
+	const chapterData = getRouteApi('/read/$book/$chapter').useLoaderData()
+
+	return (
+		<div>
+			<div className={cx(hstack({ gap: 4, p: 4 }))}>
+				<div className={css({
+					fontFamily: 'mono',
+				})}>mono font
+				</div>
+				<div className={css({
+					fontFamily: 'soft',
+				})}>soft font
+				</div>
+				<div className={css({
+					fontFamily: 'dyslexic',
+				})}>dyslexic font
+				</div>
+				<div className={css({
+					fontFamily: 'condensed',
+				})}>condensed font
+				</div>
+				<div className={css({
+					fontFamily: 'old_style',
+				})}>old_style font
+				</div>
+			</div>
+			<Link to={'/read/$book/$chapter'} params={{ book: params.book, chapter: `${parseInt(params.chapter) - 1}` }}
+						className={button({ visual: 'solid', size: 'lg' })}>Previous
+				chapter</Link>
+			<Link to={'/read/$book/$chapter'} params={{ book: params.book, chapter: `${parseInt(params.chapter) + 1}` }}
+						className={button({ visual: 'solid', size: 'lg' })}>Next
+				chapter</Link>
+			<br />
+			<div dangerouslySetInnerHTML={{ __html: chapterData.content }} />
+		</div>
+	)
+}
