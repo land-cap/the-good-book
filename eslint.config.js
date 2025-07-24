64c2840b5e58cee146e15ce443f8f027a39f6d4e
Daniel Filat <filatdaniel98@gmail.com>
2025-07-24 16:58:38 +0300

    config: adds lint rule for preventing private imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,12 @@
 import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import tsEslint from 'typescript-eslint'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 
-export default [
-	js.configs.recommended,
+export default [js.configs.recommended,
 
-	...tseslint.configs.recommended,
-	...tseslint.configs.recommendedTypeChecked,
-	{
+	...tsEslint.configs.recommended, ...tsEslint.configs.recommendedTypeChecked, {
 		languageOptions: {
 			parserOptions: {
 				project: './tsconfig.json',
@@ -19,23 +16,29 @@ export default [
 
 	{
 		plugins: {
-			react,
-			'react-hooks': reactHooks,
-			'jsx-a11y': jsxA11y,
-		},
-		rules: {
-			...react.configs.recommended.rules,
-			...reactHooks.configs.recommended.rules,
-			...jsxA11y.configs.recommended.rules,
+			react, 'react-hooks': reactHooks, 'jsx-a11y': jsxA11y,
+		}, rules: {
+			...react.configs.recommended.rules, ...reactHooks.configs.recommended.rules, ...jsxA11y.configs.recommended.rules,
 			'@typescript-eslint/no-floating-promises': 'warn',
 			'react/prop-types': 'off',
 			'react/react-in-jsx-scope': 'off',
 			'react/jsx-uses-react': 'off',
-		},
-		settings: {
+			'no-restricted-imports': ['error', {
+				patterns: [{
+					regex: '/_/',
+					message: 'Import from the private _ folder is only allowed in client.ts, server.ts or shared.ts at the same directory level.',
+				}],
+			}],
+		}, settings: {
 			react: {
 				version: 'detect',
 			},
 		},
 	},
-]
+
+	{
+		files: ['**/client.ts', '**/server.ts', '**/shared.ts'],
+		rules: {
+			'no-restricted-imports': 'off',
+		},
+	}]
