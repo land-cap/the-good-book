a4cd66a674fcba2ef42e1a2a35d6cd784840b828
Daniel Filat <filatdaniel98@gmail.com>
2025-07-25 20:52:57 +0300

    adds script for generating book list

--- eslint.config.js
+++ eslint.config.js
@@ -5,47 +5,65 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 
-export default [js.configs.recommended,
+export default [
+   js.configs.recommended,
 
-	...tsEslint.configs.recommended, ...tsEslint.configs.recommendedTypeChecked, {
-		languageOptions: {
-			parserOptions: {
-				project: './tsconfig.json',
-			},
-		},
-	},
+   ...tsEslint.configs.recommended,
+   ...tsEslint.configs.recommendedTypeChecked,
+   {
+      languageOptions: {
+         parserOptions: {
+            project: './tsconfig.json',
+         },
+      },
+   },
 
-	{
-		files: ['**/*.{ts,tsx,js,jsx,json}'],
-		plugins: {
-			react, 'react-hooks': reactHooks, 'jsx-a11y': jsxA11y, 'simple-import-sort': simpleImportSort,
-		}, rules: {
-			...react.configs.recommended.rules, ...reactHooks.configs.recommended.rules, ...jsxA11y.configs.recommended.rules,
-			// typescript
-			'@typescript-eslint/no-floating-promises': 'warn',
-			// imports
-			'no-restricted-imports': ['error', {
-				patterns: [{
-					regex: '/_/',
-					message: 'Import from the private _ folder is only allowed in client.ts, server.ts or shared.ts at the same directory level.',
-				}],
-			}],
-			'simple-import-sort/imports': 'error',
-			'simple-import-sort/exports': 'error',
-			// react
-			'react/prop-types': 'off',
-			'react/react-in-jsx-scope': 'off',
-			'react/jsx-uses-react': 'off',
-		}, settings: {
-			react: {
-				version: 'detect',
-			},
-		},
-	},
+   {
+      files: ['**/*.{ts,tsx,js,jsx,json}'],
+      plugins: {
+         react,
+         'react-hooks': reactHooks,
+         'jsx-a11y': jsxA11y,
+         'simple-import-sort': simpleImportSort,
+      },
+      rules: {
+         ...react.configs.recommended.rules,
+         ...reactHooks.configs.recommended.rules,
+         ...jsxA11y.configs.recommended.rules,
+         // typescript
+         '@typescript-eslint/no-floating-promises': 'warn',
+         '@typescript-eslint/only-throw-error': 'off',
+         // imports
+         'no-restricted-imports': [
+            'error',
+            {
+               patterns: [
+                  {
+                     regex: '/_/',
+                     message:
+                        'Import from the private _ folder is only allowed in client.ts, server.ts or shared.ts at the same directory level.',
+                  },
+               ],
+            },
+         ],
+         'simple-import-sort/imports': 'error',
+         'simple-import-sort/exports': 'error',
+         // react
+         'react/prop-types': 'off',
+         'react/react-in-jsx-scope': 'off',
+         'react/jsx-uses-react': 'off',
+      },
+      settings: {
+         react: {
+            version: 'detect',
+         },
+      },
+   },
 
-	{
-		files: ['**/client.ts', '**/server.ts', '**/shared.ts'],
-		rules: {
-			'no-restricted-imports': 'off',
-		},
-	}]
+   {
+      files: ['**/client.ts', '**/server.ts', '**/shared.ts'],
+      rules: {
+         'no-restricted-imports': 'off',
+      },
+   },
+]
--- src/routes/shell.tsx
+++ src/routes/shell.tsx
@@ -3,12 +3,11 @@ import { createFileRoute, notFound } from '@tanstack/react-router'
 import { isOnServer } from '~/utils/shared'
 
 export const Route = createFileRoute('/shell')({
-	ssr: false,
-	loader: () => {
-		if (!isOnServer()) {
-			// eslint-disable-next-line @typescript-eslint/only-throw-error
-			throw notFound({ throw: true })
-		}
-	},
-	component: () => null,
+   ssr: false,
+   loader: () => {
+      if (!isOnServer()) {
+         throw notFound({ throw: true })
+      }
+   },
+   component: () => null,
 })
