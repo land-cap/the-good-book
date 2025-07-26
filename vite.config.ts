6ba74fd8c09785f5c42dcd4422613c341b4ca8f5
Daniel Filat <filatdaniel98@gmail.com>
2025-07-26 15:30:44 +0300

    sets deployment targe to netlify in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
          projects: ['./tsconfig.json'],
       }),
       tanstackStart({
+         target: 'netlify',
          customViteReactPlugin: true,
          prerender: {
             enabled: true,
