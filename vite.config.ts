2ea2e6c2b4f0bd9b82c41146b9ab656cfc91770c
Daniel Filat <filatdaniel98@gmail.com>
2025-07-26 21:33:59 +0300

    fix: app manifest

--- vite.config.ts
+++ vite.config.ts
@@ -34,29 +34,6 @@ export default defineConfig({
       viteReact(),
       VitePWA({
          injectRegister: false,
-         manifest: {
-            name: 'The Good Book',
-            short_name: 'Good Book',
-            description: 'An offline-first Bible reading app.',
-            theme_color: '#ffffff',
-            icons: [
-               {
-                  src: '/android-chrome-192x192.png',
-                  sizes: '192x192',
-                  type: 'image/png',
-               },
-               {
-                  src: '/android-chrome-512x512.png',
-                  sizes: '512x512',
-                  type: 'image/png',
-               },
-               {
-                  src: '/apple-touch-icon.png',
-                  sizes: '180x180',
-                  type: 'image/png',
-               },
-            ],
-         },
       }),
    ],
 })
