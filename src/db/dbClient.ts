83517c3a0cedf4e84f932c6dac4afbbeca920473
Daniel Filat <filatdaniel98@gmail.com>
2025-07-26 21:10:57 +0300

    fix: errir in dbClient on netlify

--- src/db/dbClient.ts
+++ src/db/dbClient.ts
@@ -1,5 +1,4 @@
-import prismaModule from '@prisma/client'
-
-const { PrismaClient } = prismaModule
+import prismaMod from '../../prisma-generated/index.js'
+const { PrismaClient } = prismaMod
 
 export const dbClient = new PrismaClient()
